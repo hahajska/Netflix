fb61d864d8e2988dee71106cc9c3c230f6f3f025
thomasinho537 <tomas.schlogl@gym-tisnov.cz>
2021-03-15 19:52:03 +0100

    delame footer - zacatek

---
+++ src/containers/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer } from "../components";
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <Footer.Title>
+        <h1>hello gayo</h1>dont be gay pfaffals be a mna
+      </Footer.Title>
+    </Footer>
+  );
+}
