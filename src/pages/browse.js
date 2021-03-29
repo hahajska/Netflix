e7c904e15a68bf3c4740fe1d908aac689bb7c6b7
TomasSchlogl <thomas537@seznam.cz>
2021-03-29 20:04:38 +0200

    lul

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { useContent } from "../hooks";
 
 export default function Browse() {
+  const { series } = useContent("series");
+  console.log(series);
+
   return <p>hello from the Browse</p>;
 }
